bceb9e6638323a539e18fb712c39a194528912f0
Angel Tsvetkov <angel.tsvetkov@telerik.com>
2015-03-25 15:28:37 +0000

    Changes made by Angel Tsvetkov (angel.tsvetkov@telerik.com).
    fix images src

--- NativeScript Friends/app/views/activities-page.js
+++ NativeScript Friends/app/views/activities-page.js
@@ -2,16 +2,11 @@ var vmModule = require("../view-models/activities-view-model");
 var frameModule = require("ui/frame");
 var dialogs = require("ui/dialogs");
 var view = require("ui/core/view");
-var platformModule = require("platform");
 
 function pageLoaded(args) {
     
     var page = args.object;
     page.bindingContext = new vmModule.activitiesViewModel();
-    
-    if (platformModule.device.os == "Android") {
-        frameModule.topmost().android.actionBar.hide();
-    }
     
 }
 
--- NativeScript Friends/app/views/main-page.js
+++ NativeScript Friends/app/views/main-page.js
@@ -1,10 +1,15 @@
 var vmModule = require("../view-models/main-view-model");
 var frameModule = require("ui/frame");
+var platformModule = require("platform");
 
 // Event handler for Page "loaded" event attached in main-page.xml
 function pageLoaded(args) {
     var page = args.object;
     page.bindingContext = vmModule.mainViewModel;
+    
+    if (platformModule.device.os == "Android") {
+        frameModule.topmost().android.actionBar.hide();
+    }
 }
 
 function navigateRegister(args){
