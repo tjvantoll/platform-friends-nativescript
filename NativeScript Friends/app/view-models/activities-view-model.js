d45150b74eada73a211f6d39ff19f7c3126b6e96
Angel Tsvetkov <angel.tsvetkov@telerik.com>
2015-03-29 07:56:13 +0000

    Changes made by Angel Tsvetkov (angel.tsvetkov@telerik.com).
    Add activity indicator

--- NativeScript Friends/app/view-models/activities-view-model.js
+++ NativeScript Friends/app/view-models/activities-view-model.js
@@ -1,75 +1,105 @@
+var __extends = this.__extends || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    __.prototype = b.prototype;
+    d.prototype = new __();
+};
+
 var observable = require("data/observable");
 var observableArray = require("data/observable-array");
 var Everlive = require("../lib/everlive.all.min");
+var imageSource = require("image-source");
+var imageCache = require("ui/image-cache");
+var view = require("ui/core/view");
 
-var activitiesViewModel = function (source)
-{
-    this._activities = new observableArray.ObservableArray();
+var cache = new imageCache.Cache();
+cache.invalid = imageSource.fromFile("~/app/res/avatar.png");
+cache.placeholder = imageSource.fromFile("~/app/res/avatar.png");
+cache.maxRequests = 5;
+
+
+
+var ActivitiesViewModel = (function (_super){
+    
+    __extends(ActivitiesViewModel, _super);
+    
+    function ActivitiesViewModel(source) {
+        _super.call(this);
+        this._source = source;
+        this._activities = new observableArray.ObservableArray(); 
+        this.set("isLoading", true);
+    }
+
+    Object.defineProperty(ActivitiesViewModel.prototype, "activities", {
+        get: function () {
+
+            var el = new Everlive({ apiKey: BS_API_KEY });
+            var that = this;
+
+            var expandExp = {
+                "UserId": {
+                    "ReturnAs": "User",
+                    "Expand": {
+                        "Picture": "Picture"
+                    }
+                },
+                "Picture": {
+                    "ReturnAs": "Picture",
+                    "SingleField": "Uri"
+                }
+            };
+
+
+            var data = el.data('Activities');
+
+            data.expand(expandExp).get().then(function(data) {
+
+               for(var i = 0; i < data.result.length; i++){
+                   var current = data.result[i];
+                   current.dateConverter = dateConverter;
+                   current.UserName = current.User.DisplayName;
+                   current.AvatarUri = current.User.Picture == null ? null : current.User.Picture.Uri;
+               }
+
+               that._activities.push(data.result); 
+
+            }, function(error) {
+               alert("Activities can't be retrieved");
+            });      
+
+            return this._activities;        
+        },
+            enumerable: true,
+            configurable: true
+    });
+    
+    return ActivitiesViewModel;
+})(observable.Observable);
+
+exports.ActivitiesViewModel = ActivitiesViewModel;
+
+function isValidImageUrl(url) {
+    return url && (url.indexOf(".png") !== -1 || url.indexOf(".jpg") !== -1);
 }
 
 var dateConverter = {
-	toView: function (value, format) {
-		var result = format;
-		var day = value.getDate();
-		result = result.replace("DD", month < 10 ? "0" + day : day);
-		var month = value.getMonth() + 1;
-		result = result.replace("MM", month < 10 ? "0" + month : month);
-		result = result.replace("YYYY", value.getFullYear());
-		return result;
-	},
-	toModel: function (value, format) {
-		var ddIndex = format.indexOf("DD");
-		var day = parseInt(value.substr(ddIndex, 2));
-		var mmIndex = format.indexOf("MM");
-		var month = parseInt(value.substr(mmIndex, 2));
-		var yyyyIndex = format.indexOf("YYYY");
-		var year = parseInt(value.substr(yyyyIndex, 4));
-		var result = new Date(year, month - 1, day);
-		return result;
-	}
-}
-
-Object.defineProperty(activitiesViewModel.prototype, "activities", {
-    get: function () {
-        
-        var el = new Everlive({ apiKey: BS_API_KEY });
-        var that = this;
-        
-        var expandExp = {
-            "UserId": {
-                "ReturnAs": "User",
-                "Expand": {
-                    "Picture": "Picture"
-                }
-            },
-            "Picture": {
-                "ReturnAs": "Picture",
-                "SingleField": "Uri"
-            }
-        };
-        
-        
-        var data = el.data('Activities');
-        
-        data.expand(expandExp).get().then(function(data) {
-            
-           for(var i = 0; i < data.result.length; i++){
-               var current = data.result[i];
-               current.dateConverter = dateConverter;
-               current.UserName = current.User.DisplayName;
-               current.AvatarUri = current.User.Picture == null ? null : current.User.Picture.Uri;
-           }
-
-           that._activities.push(data.result); 
-            
-        }, function(error) {
-           alert("Activities can't be retrieved");
-        });      
-        
-        return this._activities;        
+    toView: function (value, format) {
+        var result = format;
+        var day = value.getDate();
+        result = result.replace("DD", month < 10 ? "0" + day : day);
+        var month = value.getMonth() + 1;
+        result = result.replace("MM", month < 10 ? "0" + month : month);
+        result = result.replace("YYYY", value.getFullYear());
+        return result;
     },
-        enumerable: true,
-        configurable: true
-});
-
-exports.activitiesViewModel = activitiesViewModel;
+    toModel: function (value, format) {
+        var ddIndex = format.indexOf("DD");
+        var day = parseInt(value.substr(ddIndex, 2));
+        var mmIndex = format.indexOf("MM");
+        var month = parseInt(value.substr(mmIndex, 2));
+        var yyyyIndex = format.indexOf("YYYY");
+        var year = parseInt(value.substr(yyyyIndex, 4));
+        var result = new Date(year, month - 1, day);
+        return result;
+    }
+}
