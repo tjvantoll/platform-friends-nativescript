6a88bfb729a29cc088ce09384c81bccf352eb65f
Teodor Bozhikov <teodor.bozhikov@telerik.com>
2015-04-01 12:19:47 +0000

    activity view back button fixes, go back with swipe

--- NativeScript Friends/app/views/activity-page.js
+++ NativeScript Friends/app/views/activity-page.js
@@ -1,23 +1,40 @@
 var vmModule = require("../view-models/activity-view-model");
 var frameModule = require("ui/frame");
+var gestures = require("ui/gestures");
 var platformModule = require("platform");
 var viewModel;
 
 function pageNavigatedTo(args) {
     var page = args.object;
-    //viewModel.set("activity", activity)
+
     viewModel = new vmModule.activityViewModel();
     viewModel.activity = page.navigationContext;
+    
     page.bindingContext = viewModel
     
-    if (platformModule.device.os == "iOS") {
+    if (platformModule.device.os === "iOS") {
         frameModule.topmost().ios.controller.navigationBarHidden = true;
     }
 }
 
 function backButtonClicked(args){
-    frameModule.topmost().navigate("app/views/activities-page");
+    goBack();
+}
+
+function onSwipe(args){
+    if (args.direction === gestures.SwipeDirection.Right) {
+        goBack();
+    }
+}
+
+function goBack(args){
+    if (frameModule.topmost().canGoBack) {
+        frameModule.topmost().goBack();
+    } else {
+        frameModule.topmost().navigate("app/views/activities-page");
+    }
 }
 
 exports.backButtonClicked = backButtonClicked;
 exports.pageNavigatedTo = pageNavigatedTo;
+exports.onSwipe = onSwipe;
